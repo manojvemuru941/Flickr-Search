dcef9dd071e048641a604f877876fac5c56e5950
Manoj Vemuru <manojvemuru@gmail.com>
2018-10-21 00:11:28 -0700

    Photo date taken and date uploaded comparable

--- MVVMFlickr/app/src/main/kotlin/com/flickr/gallery/model/FlickrImage.kt
+++ MVVMFlickr/app/src/main/kotlin/com/flickr/gallery/model/FlickrImage.kt
@@ -2,22 +2,31 @@ package com.flickr.gallery.model
 
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
+import com.flickr.gallery.utils.DATE_FORMATTER
 import com.google.gson.annotations.SerializedName
 
 /**
  * Created by Manoj Vemuru on 2018-10-19.
  */
 @Entity
-class FlickrImage {
-
+class FlickrImage : Comparable<FlickrImage> {
     @PrimaryKey(autoGenerate = true)
     var primaryId:Int = 0
     @SerializedName("id")
     var id: String? = null
     @SerializedName("url_s")
     var url_s: String? = null
+    @SerializedName("date_upload")
+    var date_upload:String? = null
     @SerializedName("datetaken")
     var datetaken : String? = null
     @SerializedName("ownername")
     var ownername : String? = null
+
+    /**
+     * Compares Image date taken and date uploaded
+     */
+    override fun compareTo(other: FlickrImage): Int {
+        return (this.date_upload?.toLong()!!.compareTo(DATE_FORMATTER.parse(other.datetaken).time))
+    }
 }
--- MVVMFlickr/app/src/main/kotlin/com/flickr/gallery/utils/Constants.kt
+++ MVVMFlickr/app/src/main/kotlin/com/flickr/gallery/utils/Constants.kt
@@ -1,6 +1,8 @@
 package com.flickr.gallery.utils
 
 import android.content.Context
+import java.text.DateFormat
+import java.text.SimpleDateFormat
 
 const val SEARCH_KEY:String = "search"
 const val METHOD_NAME_RECENT:String = "flickr.photos.getRecent"
@@ -8,6 +10,7 @@ const val METHOD_NAME_SEARCH:String = "flickr.photos.search"
 const val FORMAT:String = "json"
 const val NO_JSON_CALL_BACK:Int = 1
 const val EXTRAS:String = "url_s,date_upload,date_taken,owner_name"
+val DATE_FORMATTER : DateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
 
 private var _context:Context? = null
 
